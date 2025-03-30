69aa870bf1aef37ac00a69d32dc9a971dd62e8b8
Luke Anderson <139502319+lukeanderson03@users.noreply.github.com>
2025-03-30 16:55:14 +0000

    Added a new component and object

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import bookCover from 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1632079606i/57645345.jpg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+
+const recentBook = {
+  title: '20 Years Later',
+  author: 'Charlie Donlea',
+  date: 'December 28, 2021',
+  genre: 'Mystery',
+  cover: bookCover
+};
+
+function BookTitle() {
+  return (
+    <h3>{recentBook.title}</h3>
+  );
+}
+
+function DisplayBook() {
+  return (
+    <>
+      <h3>{recentBook.title}</h3>
+      <p>{recentBook.author}</p>
+      <p>{recentBook.date}</p>
+      <p>{recentBook.genre}</p>
+      <img src="bookCover" alt="{recentBook.title}" />
+    </>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React - MI449 Lab 9.5</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>Click the button above to change the count!</p>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BookTitle />
+      <DisplayBook />
     </>
   )
 }
