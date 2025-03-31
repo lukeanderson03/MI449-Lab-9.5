da69abe6cd52f6e287e6af4fe24e5589e00a4a0e
Luke Anderson <139502319+lukeanderson03@users.noreply.github.com>
2025-03-31 01:40:02 +0000

    Created three components and one data set

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,50 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import bookCover from 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1632079606i/57645345.jpg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Name() {
+  return (
+    <h1>Luke Anderson</h1>
+  );
+}
+
+function Subtitle() {
+  return (
+    <h2>My most recent read:</h2>
+  );
+}
 
 const recentBook = {
   title: '20 Years Later',
   author: 'Charlie Donlea',
   date: 'December 28, 2021',
   genre: 'Mystery',
-  cover: bookCover
+  cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1632079606i/57645345.jpg',
 };
-
-function BookTitle() {
-  return (
-    <h3>{recentBook.title}</h3>
-  );
-}
 
 function DisplayBook() {
   return (
     <>
       <h3>{recentBook.title}</h3>
-      <p>{recentBook.author}</p>
-      <p>{recentBook.date}</p>
-      <p>{recentBook.genre}</p>
-      <img src="bookCover" alt="{recentBook.title}" />
+      <p>Author: {recentBook.author}</p>
+      <p>Release Date: {recentBook.date}</p>
+      <p>Genre: {recentBook.genre}</p>
+      <img src={recentBook.cover} alt={recentBook.title} />
     </>
   );
 }
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <BookTitle />
+      <Name />
+      <Subtitle />
       <DisplayBook />
     </>
-  )
+  );
 }
 
 export default App
